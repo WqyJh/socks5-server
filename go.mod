3c3a11b94487a250a4cfe290bb090463932f68ce
WqyJh <781345688@qq.com>
2024-10-18 20:35:06 +0800

    fix: remove vendor

--- go.mod
+++ go.mod
@@ -1,10 +1,10 @@
-module github.com/serjs/socks5-server
+module github.com/WqyJh/socks5-server
 
 go 1.19
 
-require (
-	github.com/armon/go-socks5 v0.0.0-20160902184237-e75332964ef5
-	github.com/caarlos0/env/v6 v6.10.1
-)
+require github.com/caarlos0/env/v6 v6.10.1
 
-require golang.org/x/net v0.17.0
+require (
+	github.com/things-go/go-socks5 v0.0.5
+	golang.org/x/net v0.20.0
+)
--- server.go
+++ server.go
@@ -2,19 +2,18 @@ package main
 
 import (
 	"log"
-	"net"
 	"os"
 
-	"github.com/armon/go-socks5"
 	"github.com/caarlos0/env/v6"
+	"github.com/things-go/go-socks5"
 )
 
 type params struct {
-	User            string    `env:"PROXY_USER" envDefault:""`
-	Password        string    `env:"PROXY_PASSWORD" envDefault:""`
-	Port            string    `env:"PROXY_PORT" envDefault:"1080"`
-	AllowedDestFqdn string    `env:"ALLOWED_DEST_FQDN" envDefault:""`
-	AllowedIPs      []string  `env:"ALLOWED_IPS" envSeparator:"," envDefault:""`
+	User            string   `env:"PROXY_USER" envDefault:""`
+	Password        string   `env:"PROXY_PASSWORD" envDefault:""`
+	Port            string   `env:"PROXY_PORT" envDefault:"1080"`
+	AllowedDestFqdn string   `env:"ALLOWED_DEST_FQDN" envDefault:""`
+	AllowedIPs      []string `env:"ALLOWED_IPS" envSeparator:"," envDefault:""`
 }
 
 func main() {
@@ -25,9 +24,8 @@ func main() {
 		log.Printf("%+v\n", err)
 	}
 
-	//Initialize socks5 config
-	socks5conf := &socks5.Config{
-		Logger: log.New(os.Stdout, "", log.LstdFlags),
+	var opts = []socks5.Option{
+		socks5.WithLogger(socks5.NewLogger(log.New(os.Stdout, "", log.LstdFlags))),
 	}
 
 	if cfg.User+cfg.Password != "" {
@@ -35,26 +33,23 @@ func main() {
 			os.Getenv("PROXY_USER"): os.Getenv("PROXY_PASSWORD"),
 		}
 		cator := socks5.UserPassAuthenticator{Credentials: creds}
-		socks5conf.AuthMethods = []socks5.Authenticator{cator}
+		opts = append(opts, socks5.WithAuthMethods([]socks5.Authenticator{cator}))
 	}
 
 	if cfg.AllowedDestFqdn != "" {
-		socks5conf.Rules = PermitDestAddrPattern(cfg.AllowedDestFqdn)
+		opts = append(opts, socks5.WithRule(PermitDestAddrPattern(cfg.AllowedDestFqdn)))
 	}
 
-	server, err := socks5.New(socks5conf)
-	if err != nil {
-		log.Fatal(err)
-	}
+	server := socks5.NewServer(opts...)
 
-	// Set IP whitelist
-	if len(cfg.AllowedIPs) > 0 {
-		whitelist := make([]net.IP, len(cfg.AllowedIPs))
-		for i, ip := range cfg.AllowedIPs {
-			whitelist[i] = net.ParseIP(ip)
-		}
-		server.SetIPWhitelist(whitelist)
-	}
+	// // Set IP whitelist
+	// if len(cfg.AllowedIPs) > 0 {
+	// 	whitelist := make([]net.IP, len(cfg.AllowedIPs))
+	// 	for i, ip := range cfg.AllowedIPs {
+	// 		whitelist[i] = net.ParseIP(ip)
+	// 	}
+	// 	server.SetIPWhitelist(whitelist)
+	// }
 
 	log.Printf("Start listening proxy service on port %s\n", cfg.Port)
 	if err := server.ListenAndServe("tcp", ":"+cfg.Port); err != nil {
